a1d1b22ae684fbd0d86dd209a9d5f1d572227eaf
FlorenciaCastagnani <castagnani.flor@gmail.com>
2025-10-08 13:50:27 -0300

    Add files via upload

---
+++ tpfinalparte1/tpfinalparte1.js
@@ -0,0 +1,24 @@
+let inicio;
+let sasara = [];
+
+
+function preload(){
+  inicio = loadImage('/assets/key_resized.png')
+  sasara.push( (loadImage('.../sasara
+
+}
+
+
+function setup() {
+createCanvas(640,480);
+background(255);
+}
+
+
+function draw() {
+  
+  
+  //----------------------------------------- PANTALLA INICIO
+  
+image( inicio, 0,0);
+}
